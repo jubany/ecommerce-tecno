44c58f68db650f0142616b020973471a0fe18ffd
Alejo <jubany4@gmail.com>
2023-12-12 10:28:46 -0300

    cambios en global css y archivos index.js, app y document

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import { SessionProvider } from "next-auth/react"
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { SessionProvider } from "next-auth/react";
 
-export default function App({
-  Component, pageProps: { session, ...pageProps }
-}) {
+export default function App({Component, pageProps: { session, ...pageProps }}) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps}/>
